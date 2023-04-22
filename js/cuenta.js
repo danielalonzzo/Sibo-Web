9577183bd2633de6430c56c2b4abb301c5077145
Daniel Morales <danielalonzzo@icloud.com>
2023-04-21 22:10:32 -0600

    Avances

---
+++ js/cuenta.js
@@ -0,0 +1,10 @@
+function guardar() {
+    var nombre = document.getElementById("nombre").innerHTML;
+    var apellidos = document.getElementById("apellidos").innerHTML;
+    var email = document.getElementById("correo").innerHTML;
+    var email = document.getElementById("contraseña").innerHTML;
+    var email = document.getElementById("genero").innerHTML;
+    var email = document.getElementById("edad").innerHTML;
+
+    alert("La información ha sido actualizada");
+}
